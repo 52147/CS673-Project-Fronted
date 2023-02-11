9b9858213248c25958b4417909481a9ca812c24f
Your Name <debra4117@gmail.com>
2023-02-11 10:51:13 -0500

    Delete import Login and Link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-
 import './App.css';
-import { Login } from './pages/Login';
 import { InputCar } from './pages/InputCar';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 function App() {
   return (
