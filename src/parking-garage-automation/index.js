3d0c3b0fc806ebffe7dc4ba9d099b61bfa745c00
mingjiagong <gongmingjia233@gmail.com>
2023-02-12 21:32:46 -0500

    CSS change

--- src/parking-garage-automation/index.js
+++ src/parking-garage-automation/index.js
@@ -1,7 +1,4 @@
 import ParkingInformationComponent from "./parking-information";
-
-
-
 import {Route, Routes} from "react-router";
 import ParkingPaymentComponent from "./parking-payment";
 import {InputCar} from "./InputCar/InputCar";
