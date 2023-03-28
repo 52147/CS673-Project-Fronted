527ada1d0ec7070bbef15c16d069e3f38c5ebe77
mingjiagong <gongmingjia233@gmail.com>
2023-03-28 18:21:46 -0400

    fix parkInfo problem

--- src/parking-garage-automation/reducers/parkInfoReducer.js
+++ src/parking-garage-automation/reducers/parkInfoReducer.js
@@ -22,14 +22,16 @@ const parkInfoSlice = createSlice({
             },
         [getParkingInfoThunk.fulfilled]:
             (state, { payload }) => {
+                console.log(payload)
                 state.loading = false
-                state.PlateNumber = payload.content.parkInfo.plate
-                if(payload.content.parkInfo.entrance !=null){state.EnterTime = payload.content.parkInfo.entrance.substring(0,19)}
-                if(payload.content.exit !=null){state.DepartureTime = payload.content.exit.substring(0,19)}
-                state.TotalParkingTime = payload.content.parkingtime
+                state.PlateNumber = payload.content.parkinfo.plate
+                if(payload.content.parkinfo.entrance !=null){state.EnterTime = payload.content.parkinfo.entrance.substring(0,19)}
+                //if(payload.content.exit !=null){state.DepartureTime = payload.content.exit.substring(0,19)}
+                state.TotalParkingTime = payload.content.parking_time
                 state.ParkingFee = payload.content.parkingFee
                 state.msg = 'success'
-                // console.log(payload)
+
+                console.log(payload)
             },
         [getParkingInfoThunk.rejected]:
             (state) => {
