404245b149d5551119866311b002b997faa18bc4
Shou-Tzu Han <debra4117@gmail.com>
2023-04-13 13:20:43 -0400

    update reducer

--- src/parking-garage-automation/reducers/forgetPasswordReducer.js
+++ src/parking-garage-automation/reducers/forgetPasswordReducer.js
@@ -4,13 +4,7 @@ import {forgetPasswordThunk} from "../../services/forgetPasswordThunk";
 const initialState = {
     loading: false,
     msg: '',
-    user:{
-        username:'username',
-        q1:'1',
-        a1:'11',
-        q2:'2',
-        a2:'22',
-    },
+    user:{},
 
 
 }
@@ -18,28 +12,32 @@ const initialState = {
 const forgetPasswordSlice = createSlice({
     name: 'forgetPassword',
     initialState,
-    reducers: {
-        //...
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(forgetPasswordThunk.pending, (state) => {
+    extraReducers: {
+        [forgetPasswordThunk.pending]:
+            (state) => {
                 state.loading = true
                 //console.log("pending")
-            })
-            .addCase(forgetPasswordThunk.fulfilled, (state, { payload }) => {
+            },
+        [forgetPasswordThunk.fulfilled]:
+            (state, { payload }) => {
                 state.loading = false
                 state.msg = 'success'
-                state.user = payload.user
+                state.user = payload
                 //console.log("success")
-                //console.log(payload)
-            })
-            .addCase(forgetPasswordThunk.rejected, (state) => {
+                console.log(payload)
+            },
+        [forgetPasswordThunk.rejected]:
+            (state) => {
                 state.loading = false
                 state.msg = 'reject'
                 //console.log("reject" )
-            });
+            }
     },
+
+    reducers: {
+        //...
+    }
+
 });
 
 export default forgetPasswordSlice.reducer
--- src/parking-garage-automation/reducers/parkHistoryReducer.js
+++ src/parking-garage-automation/reducers/parkHistoryReducer.js
@@ -1,40 +1,74 @@
-import { isAnyOf, createSlice } from "@reduxjs/toolkit";
-import { getHistoryThunk, getSelectedHistoryThunk } from "../../services/parkHistoryThunk";
+import {createSlice} from "@reduxjs/toolkit";
+import {getHistoryByPlateThunk, getHistoryThunk, getSelectedHistoryThunk} from "../../services/parkHistoryThunk";
 
 const initialState = {
     loading: false,
     history: []
 }
 
+
 const parkHistorySlice = createSlice({
     name: 'parkHistory',
     initialState,
-    reducers: {
-        // ...
-    },
-    extraReducers(builder) {
-        builder
-            .addMatcher(
-                isAnyOf(getHistoryThunk.pending, getSelectedHistoryThunk.pending),
-                (state) => {
-                    state.loading = true;
-                }
-            )
-            .addMatcher(
-                isAnyOf(getHistoryThunk.fulfilled, getSelectedHistoryThunk.fulfilled),
-                (state, action) => {
-                    state.loading = false;
-                    state.history = action.payload;
-                }
-            )
-            .addMatcher(
-                isAnyOf(getHistoryThunk.rejected, getSelectedHistoryThunk.rejected),
-                (state) => {
-                    state.loading = false;
-                }
-            );
-    },
-});
+    extraReducers: {
+        [getHistoryThunk.pending]:
+            (state) => {
+                state.loading = true
+                //console.log("pending");
+            },
+        [getHistoryThunk.fulfilled]:
+            (state, { payload }) => {
+                state.loading = false
+                state.history = payload
+                //console.log(payload);
+            },
+        [getHistoryThunk.rejected]:
+            (state) => {
+                state.loading = false
+                //console.log("reject");
+            },
 
+        [getSelectedHistoryThunk.pending]:
+            (state) => {
+                state.loading = true
+                //console.log("pending");
+            },
+        [getSelectedHistoryThunk.fulfilled]:
+            (state, { payload }) => {
+                state.loading = false
+                state.history = payload
+                //console.log(payload);
+            },
+        [getSelectedHistoryThunk.rejected]:
+            (state) => {
+                state.loading = false
+                //console.log("reject");
+            },
+
+        [getHistoryByPlateThunk.pending]:
+            (state) => {
+                state.loading = true
+                console.log("pending1");
+            },
+        [getHistoryByPlateThunk.fulfilled]:
+            (state, { payload }) => {
+                state.loading = false
+                state.history = payload
+                console.log(payload);
+            },
+        [getHistoryByPlateThunk.rejected]:
+            (state) => {
+                state.loading = false
+                console.log("reject1");
+            }
+
+
+    },
+
+    reducers: {
+        //...
+    }
+
+});
 
 export default  parkHistorySlice.reducer
