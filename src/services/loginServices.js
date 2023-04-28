353528d90f180ec502fb089020fbb14356de399d
Shou-Tzu Han <debra4117@gmail.com>
2023-04-28 02:33:29 -0400

    change backend deployed url

--- src/services/loginServices.js
+++ src/services/loginServices.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const COMMENTS_API_TEST = 'http://localhost:8080'
-// const COMMENTS_API = "https://shocking-tomatoes-production.up.railway.app";
-// const COMMENTS_API = "https://shocking-tomatoes-production.up.railway.app";
+// const COMMENTS_API_TEST = 'http://localhost:8080'
+const COMMENTS_API_TEST = "https://shocking-tomatoes-production.up.railway.app";
 
 export const login = async (json) => {
     const response = await axios.post(COMMENTS_API_TEST+"/login", json)
